6ab8af774e49738d93de1cc7696d606cb67237aa
a.ploter <a.ploter@g.nsu.ru>
2020-10-23 23:25:04 +0700

    Add - Task 2

---
+++ task_2_cl.clj
@@ -0,0 +1,21 @@
+(defn get-all-primes
+  []
+  (letfn [(reinsert [table x prime]
+             (update-in table [(+ prime x)] conj prime))
+          (primes-step [table d]
+             (if-let [factors (get table d)]
+               (recur (reduce #(reinsert %1 d %2) (dissoc table d) factors)
+                      (inc d))
+               (lazy-seq (cons d (primes-step (assoc table (* d d) (list d))
+                                              (inc d))))))]
+    (primes-step {} 2)))
+
+    (take 2 (get-all-primes))
+    
+    (take 5 (get-all-primes))
+    
+    (take 9 (get-all-primes))
+    
+    (take 12 (get-all-primes))
+    
+    (take 22 (get-all-primes))
